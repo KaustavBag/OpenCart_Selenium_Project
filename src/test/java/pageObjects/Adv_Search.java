7d3abfb8387ede9c73e351b6b7c9ee130abecf09
Kaustav Bag <kaustav.sdet@gmail.com>
2024-04-20 18:45:24 +0530

    Skeleton created for Adv_Search class

--- src/test/java/pageObjects/Adv_Search.java
+++ src/test/java/pageObjects/Adv_Search.java
@@ -1,1 +1,12 @@
+package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import pageObjects.BasePage;
+
+public class Adv_Search extends BasePage {
+
+
+    public Adv_Search(WebDriver driver) {
+        super(driver);
+    }
+}q
